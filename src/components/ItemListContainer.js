b41a1c972401be69fdc93ebde9ca2fb548bff1b8
Jess More <96090633+JessicaMoretti@users.noreply.github.com>
2022-03-08 11:22:19 -0300

    se agrego onAdd

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,5 @@
 import ItemCount from "./ItemCount"
-
+const miOnAdd = () =>{}
 const ItemListContainer =(props) =>{
     
     const greeting=props.greeting
@@ -7,7 +7,7 @@ const ItemListContainer =(props) =>{
     return(
         <>
             <h1>{greeting}</h1>
-            <ItemCount initial={1} stock={7}/>
+            <ItemCount initial={1} stock={7} onAdd={miOnAdd}/>
         </>
     );
 }
